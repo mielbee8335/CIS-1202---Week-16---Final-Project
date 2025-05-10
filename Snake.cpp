4f60bbe8e823fdde73b1776e9c049760ac6c07ea
Kathleen Fillingim <kpfillingim@yahoo.com>
2025-05-10 14:07:12 -0400

    Declared and defined Snake class

---
+++ Snake.cpp
@@ -0,0 +1,62 @@
+#include "Snake.h"
+#include<string>
+
+using namespace std;
+
+// Snake class member function definitions:
+
+Snake::Snake() // Default constructor
+{
+	Pet();
+	snakeLength = 0.0;
+	isVenomous = false;
+}
+
+Snake::Snake(string& name, string& breed, double weight, double length, bool venom) // Constructor
+{
+	Pet(name, breed, weight);
+	snakeLength = length;
+	isVenomous = venom;
+}
+
+// Setters and Getters:
+
+void Snake::setSnakeLength(double length)
+{
+	snakeLength = length;
+}
+
+double Snake::getSnakeLength()
+{
+	return snakeLength;
+}
+
+void Snake::setIsVenomous(bool venom)
+{
+	isVenomous = venom;
+}
+
+string Snake::getIsVenomous()
+{
+	string status;
+	
+	if (isVenomous)
+		status = "Yes";
+	else
+		status = "No";
+
+	return status;
+}
+
+// Etc.
+
+void Snake::displayInfo(string& name, string& breed, double weight, double length, bool venom)
+{
+	Pet::displayInfo(name, breed, weight);
+
+	setSnakeLength(length);
+	cout << "\tSnake Length: " << getSnakeLength();
+
+	setIsVenomous(isVenomous);
+	cout << "\tVenomous Snake? : " << getIsVenomous();
+}
--- Snake.h
+++ Snake.h
@@ -1,1 +1,35 @@
-#pragma once
+#ifndef SNAKE_H
+#define SNAKE_H
+#include "Pet.h"
+#include<string>
+
+using namespace std;
+
+// Snake class declaration:
+
+class Snake : public Pet
+{
+private:
+	double snakeLength;
+	bool isVenomous;
+public:
+	// Constructors:
+
+	Snake();
+	Snake(string&, string&, double, double, bool);
+
+	// Setters & Getters:
+
+	void setSnakeLength(double);
+	double getSnakeLength();
+
+	void setIsVenomous(bool);
+	string getIsVenomous();
+
+	// Etc.
+
+	void displayInfo(string&, string&, double, double, bool);
+
+};
+
+#endif 
